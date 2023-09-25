9112cfee24709f2fdd5c8008edc75b1ccbda8008
Konner Horton <konnerhorton@gmail.com>
2023-09-25 13:34:43 -0400

    Update

--- Shear_Strength.py
+++ Shear_Strength.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import io
 
 # Local Libraries
 from utilities.utilities import *
 
 
 st.title("Shear Strength of Soils")
+st.write(
+    "Input the triaxial test results in the table below. When all tests are included, click the 'Calculate and plot $c$ & $\phi$' button view see the results in the space below.\nThe plot can be downloaded as a pdf (see button below the plot)."
+)
 df = pd.DataFrame(
     {
         "Specimen Number": [1, 2],
@@ -19,7 +23,7 @@ edited_df = st.experimental_data_editor(df, num_rows="dynamic")
 
 units = st.text_input("Input Stress Units", "kPa")
 
-fig = default_mohr_circle(units)
+fig = default_triaxial_plot(units=units, x_max=1000, y_max=500)
 
 if st.button("Calculate and plot $c$ & $\phi$"):
     if any(edited_df["Minor Principal Stress"] >= edited_df["Major Principal Stress"]):
@@ -27,11 +31,46 @@ if st.button("Calculate and plot $c$ & $\phi$"):
             "Update data table above so that all minor principal stresses are smaller than their respective major principal stress"
         )
     else:
-        circles = make_circles(
-            edited_df["Major Principal Stress"], edited_df["Minor Principal Stress"]
+        x_max = max(edited_df["Major Principal Stress"]) * 1.1
+        y_max = x_max / 2
+        c, phi = get_mc_failure_envelope(
+            edited_df["Minor Principal Stress"], edited_df["Major Principal Stress"]
         )
-        for c in circles:
-            fig.add_shape(c)
+        failure_envelope = mc_failure_envelope(c, phi, x_max)
+        fig = (
+            mohr_circle_plot(
+                edited_df["Major Principal Stress"],
+                edited_df["Minor Principal Stress"],
+                units="kPa",
+            )
+            .add_trace(go.Scatter(x=failure_envelope[0], y=failure_envelope[1]))
+            .add_annotation(
+                text=f"Cohesion={c}<br>Friction Angle={phi}",
+                showarrow=False,
+                x=x_max * 0.95,
+                xanchor="right",
+                y=y_max * 0.95,
+                yanchor="top",
+                align="right",
+                font_color="black",
+                bordercolor="black",
+                borderpad=4,
+                bgcolor="#FFFFFF",
+            )
+        )
 
-# """If minor < major plot circles. Make a function to add a circle to a figure, instead of making a whole new figure. Might be a reasonable use case for making a class etc."""
 st.plotly_chart(fig)
+
+# Create an in-memory buffer
+buffer = io.BytesIO()
+
+# Save the figure as a pdf to the buffer
+fig.write_image(file=buffer, format="pdf")
+
+# Download the pdf from the buffer
+st.download_button(
+    label="Download PDF",
+    data=buffer,
+    file_name="MohrCricleWithFailureEnvelope.pdf",
+    mime="application/pdf",
+)
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,3 +1,5 @@
+from statistics import linear_regression
+
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -17,109 +19,7 @@ pio.templates["simple_white"]["layout"]["font"]["family"] = "Arial"
 pio.templates.default = "simple_white"
 
 
-def default_mohr_circle(units):
-    x_max = 1000
-    y_max = (x_max / 2) * 1.1
-    fig = go.Figure()
-
-    fig.update_xaxes(range=[0, x_max], constrain="domain")
-    fig.update_yaxes(
-        range=[0, y_max],
-        scaleanchor="x",
-        scaleratio=1,
-        constrain="domain",
-    )
-    fig.update_layout(
-        margin=dict(t=40, b=20, l=20, r=20),
-        xaxis=dict(title=f"Normal Stress, {units}"),
-        yaxis=dict(title=f"Shear Stress, {units}"),
-    )
-    return fig
-
-
-def make_circles(minor, major):
-    circles = []
-    for i in zip(minor, major):
-        circles.append(
-            {
-                "type": "circle",
-                "xref": "x",
-                "yref": "y",
-                "x0": i[0],
-                "y0": (i[0] - i[1]) / 2,
-                "x1": i[1],
-                "y1": -(i[0] - i[1]) / 2,
-                "line": {"color": "black", "width": 2},
-                "fillcolor": "rgba(0,0,0,0)",
-                "opacity": 1,
-            }
-        )
-    return circles
-
-
-def draw_circles(minor_principal_stress, major_principal_stress):
-    x_max = max(major) * 1.1
-    y_max = (max(major) - minor[major.index(max(major))]) / 1.5
-
-    # def sp_points(minor, major):
-    #     minor = np.array(minor)
-    #     major = np.array(major)
-    #     q = (major - minor) / 2
-    #     p = (major + minor) / 2
-    #     return list(q), list(p)
-
-    # fig = go.Figure(
-    #     go.Scatter(
-    #         x=sp_points(minor, major)[1], y=sp_points(minor, major)[0], mode="markers"
-    #     )
-    # )
-    fig = default_mohr_circle()
-
-    for c in make_circles(minor, major):
-        fig.add_shape(c)
-
-    # fig.update_xaxes(
-    #     title=dict(text="Normal Stress or p', kPa", font_color="black", standoff=0),
-    #     range=[0, x_max],
-    #     constrain="domain",
-    #     showgrid=True,
-    #     linecolor="black",
-    #     gridcolor="lightgray",
-    #     mirror=True,
-    #     minor=dict(gridcolor="lightgray", showgrid=True),
-    # ).update_yaxes(
-    #     title=dict(text="Shear Stress or q, kPa", font_color="black", standoff=0),
-    #     range=[0, y_max],
-    #     mirror=True,
-    #     linecolor="black",
-    #     gridcolor="lightgray",
-    #     minor=dict(gridcolor="lightgray", showgrid=True),
-    #     scaleanchor="x",
-    #     scaleratio=1,
-    #     constrain="domain",
-    # ).update_layout(
-    #     plot_bgcolor="white"
-    # )
-    return fig
-
-
-def get_shear_strength_mohr_circle(
-    minor_principal_stress: list, major_principal_stress: list
-):
-    """
-    Parameters
-    ----------
-
-    Returns
-    -------
-
-    Notes
-    -----
-
-    Reference
-    ---------
-
-    """
+def get_mc_failure_envelope(minor_principal_stress: list, major_principal_stress: list):
     center = [
         (major_principal_stress[i] + minor_principal_stress[i]) / 2
         for i in range(len(major_principal_stress))
@@ -158,3 +58,98 @@ def get_shear_strength_mohr_circle(
     cohesion = round(storehouse[theta_select][1], 0)
 
     return cohesion, friction_angle
+
+
+def default_triaxial_plot(units="kPa", x_max=1000, y_max=500):
+    return (
+        go.Figure()
+        .update_xaxes(range=[0, x_max], constrain="domain")
+        .update_yaxes(
+            range=[0, y_max],
+            scaleanchor="x",
+            scaleratio=1,
+            constrain="domain",
+        )
+        .update_layout(
+            margin=dict(t=40, b=20, l=20, r=20),
+            xaxis=dict(title=f"Normal Stress, {units}"),
+            yaxis=dict(title=f"Shear Stress, {units}"),
+        )
+    )
+
+
+def mohr_circle_plot(sigma1s, sigma3s, units="kPa"):
+    x_max = max(sigma1s) * 1.1
+    y_max = x_max / 2
+    print(sigma3s)
+    fig = default_triaxial_plot(units=units, x_max=x_max, y_max=y_max)
+    for i in zip(sigma3s, sigma1s):
+        x0 = i[0]
+        x1 = i[1]
+        y0 = (x0 - x1) / 2
+        y1 = -y0
+        fig.add_shape(
+            type="circle",
+            xref="x",
+            yref="y",
+            x0=x0,
+            y0=y0,
+            x1=x1,
+            y1=y1,
+            fillcolor="rgba(0,0,0,0)",
+            opacity=1,
+            line=dict(
+                color="black",
+                width=2,
+            ),
+        )
+    return fig
+
+
+def mc_failure_envelope(c, phi, x_max):
+    x = np.linspace(0, x_max)
+    m = np.tan(np.deg2rad(phi))
+    y = m * x + c
+    return x, y
+
+
+class TriaxialSampleSet:
+    def __init__(self, sample_id, test_type, stress_units):
+        self.id = sample_id
+        self.type = test_type
+        self.units = stress_units
+        self.specimens = []
+
+    def add_specimen(self, specimen):
+        self.specimens.append(specimen)
+        self.sigma1s = [specimen.sigma1 for specimen in self.specimens]
+        self.sigma3s = [specimen.sigma3 for specimen in self.specimens]
+
+    def plot_mc_failure_envelope(self):
+        fig = mohr_circle_plot(
+            sigma1s=self.sigma1s, sigma3s=self.sigma3s, units=self.units
+        )
+        failure_envelope = mc_failure_envelope(
+            get_mc_failure_envelope(self.sigma3s, self.sigma1s)[0],
+            get_mc_failure_envelope(self.sigma3s, self.sigma1s)[1],
+            max([specimen.sigma1 for specimen in self.specimens]) * 1.1,
+        )
+
+        return fig.add_trace(go.Scatter(x=failure_envelope[0], y=failure_envelope[1]))
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "specimens": [s.as_dict() for s in self.specimens],
+        }
+
+
+class Specimen:
+    def __init__(self, specimen_id, sigma1, sigma3):
+        self.id = specimen_id
+        self.sigma1 = sigma1
+        self.sigma3 = sigma3
+
+    def as_dict(self):
+        return {"id": self.id, "sigma1": self.sigma1, "sigma3": self.sigma3}
